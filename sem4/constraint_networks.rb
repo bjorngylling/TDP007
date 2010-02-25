efcb1239e7daf71fcb53b0142ba67f6a6430ed64
Björn Gylling <bjorn.gylling@gmail.com>
2010-02-25 16:32:39 +0100

    wrote test-case for celsius2fahrenheit and cleaned up the adder code and test

--- sem4/constraint_networks.rb
+++ sem4/constraint_networks.rb
@@ -297,22 +297,6 @@ class ConstantConnector < Connector
     raise ContradictionException.new("Cannot assign a constant a value!")
   end
 end
-  
-# This is a simple test of the constraint network
-
-def test_adder
-  a = Connector.new("a")
-  b = Connector.new("b")
-  c = Connector.new("c")
-  Adder.new(a, b, c)
-  a.user_assign(10)
-  b.user_assign(5)
-  puts "c = "+c.value.to_s
-  a.forget_value "user"
-  c.user_assign(20)
-  # a should now be 15
-  puts "a = "+a.value.to_s
-end
 
 # ----------------------------------------------------------------------------
 #  Assignment
--- sem4/test_constraint.rb
+++ sem4/test_constraint.rb
@@ -1,33 +1,39 @@
 require "test/unit"
 require "constraint_networks.rb"
 
-
-
-
 class TestCN < Test::Unit::TestCase
 	def test_adder
 		a = Connector.new("a")
 		b = Connector.new("b")
 		c = Connector.new("c")
 		Adder.new(a, b, c)
+		
 		a.user_assign(10)
 		b.user_assign(5)
-		
 		assert_equal(15, c.value) 
-		
 		
 		a.forget_value "user"
 		c.user_assign(20)
-		
 		assert_equal(15, a.value)
 		
-		
 		b.forget_value "user"
+		a.user_assign(15)
 		c.user_assign(29)
-		
 		assert_equal(14, b.value)
 	end
 	
-	
+	def test_celsius2fahrenheit
+		c,f=fahrenheit2celsius
+		
+		c.user_assign 100
+		assert_equal(212, f.value)
+		
+		c.user_assign 0
+		assert_equal(32, f.value)
+		
+		c.forget_value "user"
+		f.user_assign 100
+		assert_equal(37, c.value)
+	end
 	
 end
